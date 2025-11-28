0436405da50035c50006dbb41f04f0dc4ba46c04
Jonatas Zambotti <jonataszambotti@gmail.com>
2025-11-28 17:57:28 -0300

    primeiro commit - lab vagrant

---
+++ vagrant-lab/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"a9dcf22733a735062cd44c977ad791d849bc57abbe15534c28180f7579fef2a3","vagrant_version":"2.4.9"}
